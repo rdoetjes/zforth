f0119362eb66db21f6120276e4067ad661be26dc
Raymond Doetjes <rdoetjes@phonax.com>
2024-10-09 16:59:53 +0200

    get_word debug

--- src/instructions.zig
+++ src/instructions.zig
@@ -1,19 +1,18 @@
 const std = @import("std");
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa_alloc = gpa.allocator();
-pub const OpFunction = *const fn () anyerror!void;
-var operations *std.StringHashMap(OpFunction) = undefined;
+var operations: *std.StringHashMap(OpFunction) = undefined;
 var arg_stack: *std.ArrayList(f32) = undefined;
 
+pub const OpFunction = *const fn () anyerror!void;
 pub const Op = struct {
     words: []const u8,
     op: OpFunction,
 };
 
-pub fn init_operations(operations: *std.StringHashMap(OpFunction), l_arg_stack: *std.ArrayList(f32)) !void {
+pub fn init_operations(l_operations: *std.StringHashMap(OpFunction), l_arg_stack: *std.ArrayList(f32)) !void {
     arg_stack = l_arg_stack;
-    operations = operations;
-    try operations.put("push", plus);
+    operations = l_operations;
     try operations.put("+", plus);
     try operations.put("-", minus);
     try operations.put(".", dot);
@@ -28,66 +27,66 @@ pub fn init_operations(operations: *std.StringHashMap(OpFunction), l_arg_stack: 
 }
 
 pub fn minus() !void {
-    const a = stack.*.pop();
-    const b = stack.*.pop();
-    try stack.*.append(b - a);
+    const a = arg_stack.*.pop();
+    const b = arg_stack.*.pop();
+    try arg_stack.*.append(b - a);
 }
 
 pub fn plus() !void {
-    const a = stack.*.pop();
-    const b = stack.*.pop();
-    try stack.*.append(a + b);
+    const a = arg_stack.*.pop();
+    const b = arg_stack.*.pop();
+    try arg_stack.*.append(a + b);
 }
 
 pub fn mul() !void {
-    const a = stack.*.pop();
-    const b = stack.*.pop();
-    try stack.*.append(a * b);
+    const a = arg_stack.*.pop();
+    const b = arg_stack.*.pop();
+    try arg_stack.*.append(a * b);
 }
 
 pub fn div() !void {
-    const a = stack.*.pop();
-    const b = stack.*.pop();
-    try stack.*.append(b / a);
+    const a = arg_stack.*.pop();
+    const b = arg_stack.*.pop();
+    try arg_stack.*.append(b / a);
 }
 
 pub fn dot_s() !void {
-    std.debug.print(".s <1> {d}\n", .{stack.*.items[stack.*.items.len - 1]});
+    std.debug.print(".s <1> {d}\n", .{arg_stack.*.items[arg_stack.*.items.len - 1]});
 }
 
 pub fn dot_cap_s() !void {
-    std.debug.print(".S <{d}> ", .{stack.*.items.len});
-    for (stack.*.items) |item| {
+    std.debug.print(".S <{d}> ", .{arg_stack.*.items.len});
+    for (arg_stack.*.items) |item| {
         std.debug.print("{d} ", .{item});
     }
     std.debug.print("\n", .{});
 }
 
 pub fn dot() !void {
-    const a = stack.*.pop();
+    const a = arg_stack.*.pop();
     std.debug.print("{d}\n", .{a});
 }
 
 pub fn dup() !void {
-    const a = stack.*.pop();
-    try stack.*.append(a);
-    try stack.*.append(a);
+    const a = arg_stack.*.pop();
+    try arg_stack.*.append(a);
+    try arg_stack.*.append(a);
 }
 
 pub fn drop() !void {
-    _ = stack.*.pop();
+    _ = arg_stack.*.pop();
 }
 
 pub fn swap() !void {
-    const a = stack.*.pop();
-    const b = stack.*.pop();
-    try stack.*.append(a);
-    try stack.*.append(b);
+    const a = arg_stack.*.pop();
+    const b = arg_stack.*.pop();
+    try arg_stack.*.append(a);
+    try arg_stack.*.append(b);
 }
 
 pub fn sqrt() !void {
-    const a = stack.*.pop();
-    try stack.*.append(std.math.sqrt(a));
+    const a = arg_stack.*.pop();
+    try arg_stack.*.append(std.math.sqrt(a));
 }
 
 pub fn print(a: []const u8) !void {
--- src/main.zig
+++ src/main.zig
@@ -10,31 +10,26 @@ var op_stack: *std.ArrayList(*instructions.Op) = undefined;
 
 fn get_word(start_index: *usize, line: []const u8) !struct { *usize, []const u8 } {
     const start_pos = start_index.*;
-    while (line[start_index.*] != ' ' and start_index.* < line.len - 1) {
+    while (start_index.* <= line.len - 1 and line[start_index.*] != ' ') {
         start_index.* += 1;
     }
-    return .{ start_index, line[start_pos..start_index.*] };
-}
 
+    start_index.* += 1;
+    return .{ start_index, line[start_pos .. start_index.* - 1] };
+}
 fn parse(line: []const u8) !void {
     var start_index: usize = 0;
     //this allows us to skip forward to closing parens by altering the start_index
     while (start_index < line.len) {
         const result = try get_word(&start_index, line);
-        start_index = result[0].* + 1;
+        start_index = result[0].*;
         const word = result[1];
 
         //this is the only situation where we use a capitalized word, the rest can be any case
         if (std.mem.eql(u8, word, ".S")) {
             if (operations.*.get(word)) |op| {
                 try op();
-            }
-        }
-
-        if (word[0] == ':') {
-            std.debug.print("{s}", .{line[start_index .. line.len - 1]});
-            if (!std.mem.containsAtLeast(u8, line[start_index..line.len], 1, " ;")) {
-                return error.Colon_Without_Semicolon;
+                continue;
             }
         }
 
@@ -42,9 +37,9 @@ fn parse(line: []const u8) !void {
         _ = std.ascii.lowerString(lower_word, word);
 
         if (operations.*.get(lower_word)) |op| {
-            op = gpa_alloc.create(instructions) catch unreachable;
-            op.*.op = op;
-            try op_stack.*.append(op);
+            const op_struct = try gpa_alloc.create(instructions.Op);
+            op_struct.*.op = op;
+            try op_stack.*.append(op_struct);
         } else {
             try arg_stack.*.append(try std.fmt.parseFloat(f32, word));
         }
@@ -52,20 +47,33 @@ fn parse(line: []const u8) !void {
         gpa_alloc.free(lower_word);
     }
 }
+
+fn compile() !void {
+    for (op_stack.items) |op| {
+        if (op.*.op != undefined) {
+            try op.*.op();
+        } else if (op.*.words.len > 0) {
+            try parse(op.*.words);
+            try compile();
+        }
+    }
+}
+
 pub fn main() !void {
     var l_arg_stack = std.ArrayList(f32).init(gpa_alloc);
     defer l_arg_stack.deinit();
     arg_stack = &l_arg_stack;
 
-    var l_op_stack = std.ArrayList(instructions.Op).init(gpa_alloc);
+    var l_op_stack = std.ArrayList(*instructions.Op).init(gpa_alloc);
     defer l_op_stack.deinit();
     op_stack = &l_op_stack;
 
-    var operations_local: std.StringHashMap(instructions.OpFunction) = undefined;
-    defer operations_local.deinit();
-    operations = &operations_local;
+    var l_operations_local = std.StringHashMap(instructions.OpFunction).init(gpa_alloc);
+    defer l_operations_local.deinit();
+    operations = &l_operations_local;
 
-    try instructions.init_operations(&operations_local, &l_arg_stack);
+    try instructions.init_operations(&l_operations_local, &l_arg_stack);
 
-    try parse("25 25 * .s :test dup * ;");
+    try parse("25 25 * .S");
+    try compile();
 }
