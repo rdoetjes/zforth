cb6694c3702425ba8c87af1d4161a8b3278fe2c3
Raymond Doetjes <rdoetjes@phonax.com>
2024-10-11 22:14:50 +0200

    end token matched

--- src/main.zig
+++ src/main.zig
@@ -1,75 +1,18 @@
 const std = @import("std");
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa_alloc = gpa.allocator();
-const interpreter = @import("interpreter.zig");
-const instructions = @import("instructions.zig");
-const outw = std.io.getStdOut().writer();
-const inr = std.io.getStdIn().reader();
-
-var op_stack: *std.ArrayList(*instructions.Op) = undefined;
-
-fn prompt() void {
-    outw.print(" ok \n", .{}) catch {
-        std.debug.print("failed to print prompt", .{});
-    };
-}
-
-fn welcome() void {
-    outw.print("Small lean forth in zig\n", .{}) catch {
-        std.debug.print("failed to print welcome", .{});
-    };
-}
-
-fn repl() void {
-    welcome();
-    while (true) {
-        prompt();
-        const line = inr.readUntilDelimiterOrEofAlloc(gpa_alloc, '\n', 4096) catch |err| {
-            outw.print("{}\n", .{err}) catch {};
-            continue;
-        } orelse {
-            outw.print("End of input reached\n", .{}) catch {};
-            break;
-        };
-
-        const line_cleaned = std.mem.trim(u8, line, " \t\n");
-
-        interpreter.parse(line_cleaned) catch |err| {
-            op_stack.*.clearRetainingCapacity();
-            outw.print("{}\n", .{err}) catch {
-                std.debug.panic("failed to print error message", .{});
-            };
-        };
-
-        interpreter.compile() catch |err| {
-            op_stack.*.clearRetainingCapacity();
-            outw.print("{}\n", .{err}) catch {
-                std.debug.panic("failed to print error message", .{});
-            };
-        };
-        gpa_alloc.free(line);
-    }
-}
+const lexer = @import("lexer.zig");
 
 pub fn main() !void {
-    var l_arg_stack = std.ArrayList(f32).init(gpa_alloc);
-    defer l_arg_stack.deinit();
+    while (true) {
+        // line = try std.io.getStdIn().reader().readUntilDelimiterAlloc(gpa_alloc, '\n', 1024);
+        const line = "PUSH .\"\n";
 
-    var l_op_stack = std.ArrayList(*instructions.Op).init(gpa_alloc);
-    defer l_op_stack.deinit();
-    op_stack = &l_op_stack;
-    op_stack.*.clearRetainingCapacity();
+        const tokens = lexer.lex(gpa_alloc, line) catch |err| {
+            std.debug.panic("{any}", .{err});
+        };
 
-    var l_my_words = std.StringHashMap(*instructions.Op).init(gpa_alloc);
-    defer l_my_words.deinit();
-
-    var l_system_words = std.StringHashMap(instructions.OpFunction).init(gpa_alloc);
-    defer l_system_words.deinit();
-
-    var l_compile_words = std.StringHashMap(instructions.OpCompileFunction).init(gpa_alloc);
-    defer l_compile_words.deinit();
-
-    try interpreter.init_operations(&l_system_words, &l_arg_stack, &l_op_stack, &l_my_words, &l_compile_words);
-
-    repl();
+        lexer.print_tokens(tokens);
+        tokens.deinit();
+    }
 }
