cd1024903b7e1865d8a1ad572fc7b4e757972f05
Raymond Doetjes <rdoetjes@phonax.com>
2024-10-12 11:11:43 +0200

    lean no list parser

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,93 +1,113 @@
 const std = @import("std");
 
-const TokenType = enum {
-    PUSH,
-    POP,
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    NUMBER,
-    DOT,
-    DOT_S,
-    DOT_CAP_S,
-    DOT_DOUBLE_QUOTE,
-    EOF,
-};
+pub const lexer = struct {
+    stack: std.ArrayList(f32),
+    allocator: std.mem.Allocator,
 
-const Token = struct {
-    type: TokenType,
-    start_pos: usize,
-    text: []const u8,
-
-    pub fn init(token_type: TokenType, start_pos: usize, text: []const u8) Token {
-        return Token{
-            .type = token_type,
-            .start_pos = start_pos,
-            .text = text,
+    pub fn init(allocator: std.mem.Allocator) !*lexer {
+        const self = try allocator.create(lexer);
+        self.* = .{
+            .stack = std.ArrayList(f32).init(allocator),
+            .allocator = allocator,
         };
+        return self;
     }
-};
 
-pub fn print_tokens(tokens: *std.ArrayList(Token)) void {
-    for (tokens.*.items) |token| {
-        std.debug.print("{any} {s} {d}\n", .{ token.type, token.text, token.start_pos });
+    pub fn deinit(self: *lexer) void {
+        self.stack.deinit();
+        std.heap.page_allocator.destroy(self);
     }
-}
 
-fn peek(line: []const u8, pos: usize) u8 {
-    if (pos + 1 < line.len) return line[pos + 1] else return 0;
-}
-
-fn skip_white_spaces(line: *const []const u8, pos: usize) usize {
-    var t_pos = pos;
-    while (t_pos < line.*.len and (line.*[t_pos] == ' ' or line.*[t_pos] == '\n')) {
-        t_pos += 1;
+    fn peek(line: []const u8, pos: usize) u8 {
+        if (pos + 1 < line.len) return line[pos + 1] else return 0;
     }
-    return t_pos;
-}
 
-fn find_end_current_token(line: *const []const u8, pos: usize) usize {
-    var end_pos = pos;
-    while (end_pos < line.*.len and line.*[end_pos] != ' ' and line.*[end_pos] != '\n') {
-        end_pos += 1;
+    fn skip_white_spaces(line: *const []const u8, pos: usize) usize {
+        var t_pos = pos;
+        while (t_pos < line.*.len and (line.*[t_pos] == ' ' or line.*[t_pos] == '\n')) {
+            t_pos += 1;
+        }
+        return t_pos;
     }
-    return end_pos;
-}
 
-fn check_if_number(line: *const []const u8) !bool {
-    var t_pos: usize = 0;
-    while (t_pos < line.*.len and line.*[t_pos] != ' ' and line.*[t_pos] != '\n') {
-        if (line.*[t_pos] < '0' or line.*[t_pos] > '9') return error.Not_A_Number;
-        t_pos += 1;
+    fn find_end_current_token(line: *const []const u8, pos: usize) usize {
+        var end_pos = pos;
+        while (end_pos < line.*.len and line.*[end_pos] != ' ' and line.*[end_pos] != '\n') {
+            end_pos += 1;
+        }
+        return end_pos;
     }
-    return true;
-}
 
-pub fn lex(allocator: std.mem.Allocator, line: []const u8) !*std.ArrayList(Token) {
-    var tokens = try std.ArrayList(Token).initCapacity(allocator, 10);
-
-    var pos: usize = 0;
-    while (pos < line.len) {
-        pos = skip_white_spaces(&line, pos);
-        if (pos >= line.len) break;
-        const end_pos = find_end_current_token(&line, pos);
-
-        // Match token
-        const token_text = line[pos..end_pos];
-        if (std.mem.eql(u8, token_text, "+")) {
-            try tokens.append(Token.init(TokenType.ADD, pos, token_text));
-        } else if (std.mem.eql(u8, token_text, ".\"")) {
-            try tokens.append(Token.init(TokenType.DOT_DOUBLE_QUOTE, pos, token_text));
-        } else if (std.mem.eql(u8, token_text, ".")) {
-            try tokens.append(Token.init(TokenType.DOT, pos, token_text));
-        } else {
-            // Handle other token types or unknown tokens here
-            if (try check_if_number(&line[pos..end_pos])) try tokens.append(Token.init(TokenType.NUMBER, pos, token_text));
+    fn find_end_marker(line: *const []const u8, pos: usize, marker: []const u8) !usize {
+        if (std.mem.indexOf(u8, line.*[pos + 1 ..], marker)) |t_pos| {
+            return pos + t_pos + 1;
         }
 
-        pos = end_pos;
+        return error.Marker_Not_Found;
     }
 
-    return &tokens;
-}
+    fn check_if_number(line: *const []const u8) !bool {
+        var t_pos: usize = 0;
+        while (t_pos < line.*.len and line.*[t_pos] != ' ' and line.*[t_pos] != '\n') {
+            if (line.*[t_pos] < '0' or line.*[t_pos] > '9') return error.Not_A_Number;
+            t_pos += 1;
+        }
+        return true;
+    }
+
+    fn pop(self: *lexer) !f32 {
+        if (self.stack.items.len == 0) {
+            return error.Stack_Underflow;
+        }
+        const a = self.stack.pop();
+        return a;
+    }
+
+    fn add(self: *lexer) !void {
+        const b = self.stack.pop();
+        const a = self.stack.pop();
+        try self.stack.append(a + b);
+    }
+
+    fn mul(self: *lexer) !void {
+        const a = self.stack.pop();
+        const b = self.stack.pop();
+        try self.stack.append(a * b);
+    }
+
+    pub fn lex(self: *lexer, line: []const u8) !void {
+        var pos: usize = 0;
+
+        while (pos < line.len) {
+            pos = skip_white_spaces(&line, pos);
+            if (pos >= line.len) break;
+            var end_pos = find_end_current_token(&line, pos);
+
+            // Match token
+            const token_text = line[pos..end_pos];
+
+            if (std.mem.eql(u8, token_text, "+")) {
+                try self.add();
+            } else if (std.mem.eql(u8, token_text, ".\"")) {
+                const new_end_pos = try find_end_marker(&line, end_pos, "\" ");
+                const arg = line[end_pos + 1 .. new_end_pos];
+                std.debug.print("{s}", .{arg});
+                end_pos = new_end_pos + 1;
+            } else if (std.mem.eql(u8, token_text, ".")) {
+                std.debug.print("{d}", .{self.stack.pop()});
+            } else if (std.mem.eql(u8, token_text, "cr")) {
+                std.debug.print("\n", .{});
+            } else if (std.mem.eql(u8, token_text, "*")) {
+                try self.mul();
+            } else {
+                // Handle other token types or unknown tokens here
+                if (try check_if_number(&line[pos..end_pos])) {
+                    const number = try std.fmt.parseFloat(f32, token_text);
+                    try self.stack.append(number);
+                }
+            }
+
+            pos = end_pos;
+        }
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,20 @@
 const std = @import("std");
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa_alloc = gpa.allocator();
-const lexer = @import("lexer.zig");
+const lexer = @import("lexer.zig").lexer;
 const outw = std.io.getStdOut().writer();
 
 pub fn main() !void {
+    const forth = try lexer.init(gpa_alloc);
+    defer (forth.deinit());
+
     while (true) {
-        // line = try std.io.getStdIn().reader().readUntilDelimiterAlloc(gpa_alloc, '\n', 1024);
-        const line = "10 20 + .\n";
+        //const line = try std.io.getStdIn().reader().readUntilDelimiterAlloc(gpa_alloc, '\n', 1024);
+        const line = "10 20 .\" the answer is: \" * . cr";
 
-        const tokens = lexer.lex(gpa_alloc, line) catch |err| {
-            try outw.print(">>{any}\n", .{err});
-            continue;
+        forth.lex(line) catch |err| {
+            std.debug.print("Error: {any}\n", .{err});
         };
-
-        lexer.print_tokens(tokens);
-        tokens.deinit();
+        break;
     }
 }
