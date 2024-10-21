6e381e8720ba1e5774a9ab8dc65e88da5b545656
Raymond Doetjes <rdoetjes@phonax.com>
2024-10-21 15:43:05 +0200

    immediate_words into own file

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const outw = std.io.getStdOut().writer();
+const immediate_words = @import("immediate_words.zig");
 
 const ImmediateFunction = *const fn (*Interpreter) anyerror!void;
 const CompiledFunction = *const fn (*Interpreter, []const u8, *usize) anyerror!void;
@@ -20,7 +21,7 @@ pub const Stack = struct {
     }
 
     // Add methods like push, pop, peek, etc.
-    fn pop(self: *Stack) !f32 {
+    pub fn pop(self: *Stack) !f32 {
         if (self.stack_value.items.len == 0) {
             return error.Stack_Underflow;
         }
@@ -28,7 +29,7 @@ pub const Stack = struct {
         return a;
     }
 
-    fn append(self: *Stack, float: f32) !void {
+    pub fn append(self: *Stack, float: f32) !void {
         try self.stack_value.append(float);
         return;
     }
@@ -62,35 +63,6 @@ pub const Interpreter = struct {
     stack: Stack,
     dictionary: Dictionary,
 
-    fn initImmediateWords(self: *Interpreter) !void {
-        try self.dictionary.immediate_words.put(".", dot);
-        try self.dictionary.immediate_words.put("cr", cr);
-        try self.dictionary.immediate_words.put("dup", dup);
-        try self.dictionary.immediate_words.put("+", add);
-        try self.dictionary.immediate_words.put("*", mul);
-        try self.dictionary.immediate_words.put("/", div);
-        try self.dictionary.immediate_words.put(".s", dot_s);
-        try self.dictionary.immediate_words.put(".S", dot_cap_s);
-        try self.dictionary.immediate_words.put("over", over);
-        try self.dictionary.immediate_words.put("swap", swap);
-        try self.dictionary.immediate_words.put("rot", rot);
-        try self.dictionary.immediate_words.put("drop", drop);
-        try self.dictionary.immediate_words.put("=", equal);
-        try self.dictionary.immediate_words.put(">", greater);
-        try self.dictionary.immediate_words.put(">=", greater_or_equal);
-        try self.dictionary.immediate_words.put("<=", less_or_equal);
-        try self.dictionary.immediate_words.put("<", less);
-        try self.dictionary.immediate_words.put("loop", do_nothing);
-        try self.dictionary.immediate_words.put("then", do_nothing);
-        try self.dictionary.immediate_words.put("else", do_nothing);
-        try self.dictionary.immediate_words.put("begin", do_nothing);
-        try self.dictionary.immediate_words.put("until", until);
-        try self.dictionary.immediate_words.put("bye", exit);
-        try self.dictionary.immediate_words.put("words", words);
-        try self.dictionary.immediate_words.put("emit", emit);
-        try self.dictionary.immediate_words.put("rnd", rnd);
-    }
-
     fn initCompiledWords(self: *Interpreter) !void {
         try self.dictionary.compiled_words.put(".\"", print_string);
         try self.dictionary.compiled_words.put(":", compile_word);
@@ -110,7 +82,7 @@ pub const Interpreter = struct {
             .break_flag = false,
         };
 
-        try self.initImmediateWords();
+        try immediate_words.initImmediateWords(self);
         try self.initCompiledWords();
 
         return self;
@@ -183,157 +155,6 @@ pub const Interpreter = struct {
         return true;
     }
 
-    fn do_nothing(_: *Interpreter) anyerror!void {}
-
-    fn exit(_: *Interpreter) !void {
-        std.process.exit(0);
-    }
-
-    fn rnd(self: *Interpreter) !void {
-        var prng = std.rand.DefaultPrng.init(blk: {
-            var seed: u64 = undefined;
-            try std.posix.getrandom(std.mem.asBytes(&seed));
-            break :blk seed;
-        });
-        const rand = prng.random();
-
-        const a: u32 = @intFromFloat(try self.stack.pop());
-        const b: u32 = @intFromFloat(try self.stack.pop());
-        const result = rand.intRangeAtMost(u32, b, a);
-        try self.stack.append(@floatFromInt(result));
-    }
-
-    fn words(self: *Interpreter) !void {
-        try outw.print("Buildin words:\n", .{});
-        var iter = self.dictionary.immediate_words.keyIterator();
-        while (iter.next()) |key| {
-            try outw.print("{s} ", .{key.*});
-        }
-        var iter1 = self.dictionary.compiled_words.keyIterator();
-        while (iter1.next()) |key| {
-            try outw.print("{s} ", .{key.*});
-        }
-
-        try outw.print("\nExternal words:\n", .{});
-        var iter2 = self.dictionary.user_words.keyIterator();
-        while (iter2.next()) |key| {
-            try outw.print("{s} ", .{key.*});
-        }
-    }
-
-    fn over(self: *Interpreter) !void {
-        const a = try self.stack.pop();
-        const b = try self.stack.pop();
-        try self.stack.append(b);
-        try self.stack.append(a);
-        try self.stack.append(b);
-    }
-
-    fn swap(self: *Interpreter) !void {
-        const a = try self.stack.pop();
-        const b = try self.stack.pop();
-        try self.stack.append(a);
-        try self.stack.append(b);
-    }
-
-    fn rot(self: *Interpreter) !void {
-        const a = try self.stack.pop();
-        const b = try self.stack.pop();
-        const c = try self.stack.pop();
-
-        try self.stack.append(b);
-        try self.stack.append(a);
-        try self.stack.append(c);
-    }
-
-    fn drop(self: *Interpreter) !void {
-        _ = try self.stack.pop();
-    }
-
-    fn dup(self: *Interpreter) !void {
-        if (self.stack.stack_value.items.len == 0) {
-            return error.Stack_Underflow;
-        }
-        const a = try self.stack.pop();
-        try self.stack.append(a);
-        try self.stack.append(a);
-    }
-
-    fn add(self: *Interpreter) !void {
-        const b = try self.stack.pop();
-        const a = try self.stack.pop();
-        try self.stack.append(a + b);
-    }
-
-    fn mul(self: *Interpreter) !void {
-        const a = try self.stack.pop();
-        const b = try self.stack.pop();
-        try self.stack.append(a * b);
-    }
-
-    fn div(self: *Interpreter) !void {
-        const b = try self.stack.pop();
-        const a = try self.stack.pop();
-        try self.stack.append(a / b);
-    }
-
-    fn dot_cap_s(self: *Interpreter) !void {
-        try outw.print("S <{d}> ", .{self.stack.stack_value.items.len});
-        for (self.stack.stack_value.items) |item| {
-            try outw.print("{d} ", .{item});
-        }
-        try outw.print("\n", .{});
-    }
-
-    fn dot_s(self: *Interpreter) !void {
-        if (self.stack.stack_value.items.len == 0) return error.Stack_Underflow;
-        try outw.print("s {d} ", .{self.stack.stack_value.items[self.stack.stack_value.items.len - 1]});
-    }
-
-    fn dot(self: *Interpreter) !void {
-        try outw.print("{d}", .{try self.stack.pop()});
-    }
-
-    fn cr(_: *Interpreter) !void {
-        try outw.print("\n", .{});
-    }
-
-    fn equal(self: *Interpreter) !void {
-        const b = try self.stack.pop();
-        const a = try self.stack.pop();
-        try self.stack.append(if (a == b) -1 else 0);
-    }
-
-    fn greater(self: *Interpreter) !void {
-        const b = try self.stack.pop();
-        const a = try self.stack.pop();
-        try self.stack.append(if (a > b) -1 else 0);
-    }
-
-    fn less_or_equal(self: *Interpreter) !void {
-        const b = try self.stack.pop();
-        const a = try self.stack.pop();
-        try self.stack.append(if (a <= b) -1 else 0);
-    }
-
-    fn greater_or_equal(self: *Interpreter) !void {
-        const b = try self.stack.pop();
-        const a = try self.stack.pop();
-        try self.stack.append(if (a >= b) -1 else 0);
-    }
-
-    fn less(self: *Interpreter) !void {
-        const b = try self.stack.pop();
-        const a = try self.stack.pop();
-        try self.stack.append(if (a < b) -1 else 0);
-    }
-
-    fn emit(self: *Interpreter) !void {
-        const a = try self.stack.pop();
-        const char: u8 = @intFromFloat(a);
-        try outw.print("{c}", .{char});
-    }
-
     fn print_string(_: *Interpreter, line: []const u8, end_pos: *usize) !void {
         const new_end_pos = try find_end_marker(&line, end_pos.*, "\"");
         const start_pos = end_pos.*;
@@ -370,13 +191,6 @@ pub const Interpreter = struct {
             try self.lex(arg);
         }
         end_pos.* = new_end_pos + 5;
-    }
-
-    fn until(self: *Interpreter) anyerror!void {
-        const a = try self.stack.pop();
-        if (a == -1) {
-            self.break_flag = true;
-        }
     }
 
     fn if_then(self: *Interpreter, line: []const u8, end_pos: *usize) anyerror!void {
