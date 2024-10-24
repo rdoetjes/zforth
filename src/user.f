77f508ade4b0ca20fa7bda5e1b9c1d6ddc6909b4
Raymond Doetjes <rdoetjes@phonax.com>
2024-10-24 19:36:56 +0200

    demo

--- src/user.f
+++ src/user.f
@@ -20,6 +20,6 @@
 : 10printcolor repeat 30 38 rnd text_color ." Hello world " begin ;
 
 : bin16 1 16 0 do dup rot over over & rot = if ." 1" else ." 0" then swap 1 rol loop 2drop cr ;
-: kitt_up repeat 1 rol 40 ms dup 27 emit ." [10;10f" bin16 dup 32768 = until ;
-: kitt_down repeat 1 ror 40 ms dup 27 emit ." [10;10f" bin16 dup 1 = until ;
+: kitt_up repeat 1 rol 30 ms dup 27 emit ." [10;10f" bin16 dup 32768 = until ;
+: kitt_down repeat 1 ror 30 ms dup 27 emit ." [10;10f" bin16 dup 1 = until ;
 : kitt 1 repeat kitt_up kitt_down begin ;
