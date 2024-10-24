c299f4b528020d591f62c2efc5a8de8e53085eab
Raymond Doetjes <rdoetjes@phonax.com>
2024-10-24 19:22:04 +0200

    example updates with new order of loop

--- src/user.f
+++ src/user.f
@@ -9,7 +9,7 @@
 : grey 8 ;
 : white 9 ;
 : text_color 27 emit ." [" . ." m" ;
-: resistor_in_ohms rot 10 * rot + 1 rot 0 swap do 10 * loop * ;
+: resistor_in_ohms rot 10 * rot + 1 rot 0 do 10 * loop * ;
 : resistor_in_human_readible_form 32 text_color dup 1000000 >= if 1000000 / . ." M ohm" else dup 1000 >= if 1000 / . ." k Ohm" else . ."  ohm" then then 0 text_color ;
 : resistor resistor_in_ohms resistor_in_human_readible_form ;
 
